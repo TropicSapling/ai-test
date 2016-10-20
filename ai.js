25fb427a500f55d28946f7516ac2b84407527aad
Tropic <tropical.productions.scratch@gmail.com>
2016-10-20 21:26:19 +0200

    Update ai.js

--- ai.js
+++ ai.js
@@ -1,4 +1,4 @@
-var c = document.getElementByTagName("canvas");
+var c = document.getElementsByTagName("canvas");
 var ctx = c.getContext("2d");
 
 ctx.fillRect(0, canvas.height - 200, canvas.width, canvas.height);
