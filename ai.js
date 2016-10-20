0eda9b62366393631e3399afdf05aaab73ea81f8
Tropic <tropical.productions.scratch@gmail.com>
2016-10-20 22:15:47 +0200

    Update ai.js

--- ai.js
+++ ai.js
@@ -2,5 +2,5 @@ document.addEventListener('DOMContentLoaded', function() {
   var canvas = document.getElementById("game");
   var game = canvas.getContext("2d");
   
-  game.fillRect(0, 0, 200, 200);
+  game.fillRect(0, 0, 100, 100);
 }, false);
