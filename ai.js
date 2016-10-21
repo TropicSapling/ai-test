3640f65a134c89053d17f448e1da3e091b37314e
Tropic <tropical.productions.scratch@gmail.com>
2016-10-22 00:13:47 +0200

    Update ai.js

--- ai.js
+++ ai.js
@@ -7,6 +7,7 @@ var objdy = Math.abs(objy - window.innerHeight) - 250;
 var speed = 1;
 var jumping = false;
 var falling = false;
+var quickfalling = false;
 
 var operations = ["+", "-", "*", "/", Math.PI, Math.E, objx, objy, objdx, objdy, speed, "Math.abs(", "Math.acos(", "Math.asin(", "Math.atan(", "Math.atan2(", "Math.ceil(", "Math.cos(", "Math.exp(", "Math.floor(", "Math.log(", "Math.max(", "Math.min(", "Math.pow(", "Math.random(", "Math.round(", "Math.sin(", "Math.sqrt(", "Math.tan(", "(", ")", ","];
 
@@ -62,6 +63,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var action = think();
     if(action == 0) {
       jumping = true;
+    } else if(action == 1) {
+      quickFalling = true;
     }
     
     speed = speed * 1.00002;
