01d856c2ab943ba2b9aa749e0ce787fdf93d935a
Tropic <tropical.productions.scratch@gmail.com>
2016-10-21 19:30:15 +0200

    Update ai.js

--- ai.js
+++ ai.js
@@ -28,13 +28,13 @@ function jump() {
     
     for(i = 0; i < 10; i++) {
       setTimeout(function() {
-        y++;
+        y--;
       }, 5);
     }
     
     for(i = 0; i < 10; i++) {
       setTimeout(function() {
-        y--;
+        y++;
       }, 5);
     }
     
