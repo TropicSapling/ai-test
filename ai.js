8f333642fe0b409b60b48d0d98adb30f4d92b4b2
Tropic <tropical.productions.scratch@gmail.com>
2016-10-22 21:21:12 +0200

    Update ai.js

--- ai.js
+++ ai.js
@@ -29,6 +29,7 @@ function findVal(paranthesis, op) {
     var randBracket = Math.round(Math.random());
     if(!(paranthesis < 1 && randBracket == operators.indexOf(")"))) {
       genes.push(operators[randBracket]);
+      alert(randBracket);
       return operators[randBracket];
     } else {
       genes.push(operators[randOp]);
