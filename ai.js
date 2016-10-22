f41aa3816ab812814af96edc16c9532db906e85d
Tropic <tropical.productions.scratch@gmail.com>
2016-10-22 11:28:33 +0200

    Update ai.js

--- ai.js
+++ ai.js
@@ -69,7 +69,7 @@ document.addEventListener('DOMContentLoaded', function() {
       quickFalling = true;
     }
 
-    if((objx + objdx) >= x && objx <= x) {
+    if((objx + objdx) >= x && objx <= x && objy >= y && (objy - objdy) <= y) {
       // Touching obstacle
     }
     
