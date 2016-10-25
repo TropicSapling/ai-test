7e9e1c50a3e75dd032d6951206fab6db342ee3ad
Tropic <tropical.productions.scratch@gmail.com>
2016-10-25 09:02:06 +0200

    Preparing for better intervals

--- ai.js
+++ ai.js
@@ -23,6 +23,25 @@ var last_speed = 5;
 var best_speed = 5;
 var best_speed_2nd = 5;
 
+
+function interval(func, wait, times){
+  var interv = function(w, t){
+    return function(){
+      if(typeof t === "undefined" || t-- > 0){
+        setTimeout(interv, w);
+        try {
+          func.call(null);
+        } catch(e){
+          t = 0;
+          throw e.toString();
+        }
+      }
+    };
+  }(wait, times);
+  
+  setTimeout(interv, wait);
+};
+
 function findVal(parenthesis, op) {
   var randBracket = Math.round(Math.random());
   
