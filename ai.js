cdcc4ae1a3f24d947c5eae6aeafe7dfeb6c16544
Tropic <tropical.productions.scratch@gmail.com>
2016-10-26 20:47:23 +0200

    Fixed highscore not updating dynamically

--- ai.js
+++ ai.js
@@ -287,6 +287,10 @@ $(function() {
       objy = window.innerHeight - (275 + Math.round(Math.random() * 50));
       objdy = Math.abs(objy - window.innerHeight) - 250;
     }
+
+    if(speed > best_speed) {
+      highscore = Math.round(speed * 10000) - last_speed * 10000;
+    }
     
     speed = speed * 1.00002;
     
